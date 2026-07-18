f1f201604a600c81685c95ed8a4d72649a79c04a
MBello21 <magarbello@gmail.com>
2026-07-18 12:06:34 +0200

    fix modal

--- src/features/weather/components/modal/Modal.jsx
+++ src/features/weather/components/modal/Modal.jsx
@@ -8,10 +8,10 @@ const ModalContent = ({ onClose }) => {
     const { ref, handleSubmit } = useFormContext();
 
     return (
-        <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center">
+        <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4">
             <div
                 ref={ref}
-                className="bg-neutral-800 rounded-md p-6 w-1/2 flex flex-col justify-center items-center h-auto"
+                className="bg-neutral-800 rounded-md p-6 w-1/2 flex flex-col items-center h-auto max-h-[90vh] overflow-y-auto"
             >
                 <ModalHeader onClose={onClose} title="Nueva entrada" />
                 <ModalForm />
